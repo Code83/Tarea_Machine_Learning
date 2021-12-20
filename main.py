68ae373da622200c2865b4d1eb06d41a7b34fdf1
Code83 <gonzalo.guajardodiaz@outlook.com>
2021-12-20 11:48:45 -0300

    dev

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from sklearn.model_selection import train_test_split
 
 df = pad.read_csv('data/data_banknote_authentication.txt', sep=",", header=None)
 df.head()
+X, y = mglearn.datasets.make_forge()
+X_train, X_test, y_train, y_test = train_test_split(X,y)
 
 print(df)
 #print(df.shape)
