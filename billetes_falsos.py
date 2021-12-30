1ec21477ab934df5930df841ebdd9cfcf3b8e381
Code83 <codemaster83@icloud.com>
2021-12-30 15:53:38 -0300

    tarea pero con otro código y gráficos

--- billetes_falsos.py
+++ billetes_falsos.py
@@ -3,20 +3,22 @@ import pandas as pad
 import numpy as np
 import mglearn
 from IPython.display import display
+from scipy.sparse.construct import random
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 import matplotlib.pyplot as plt
 from sklearn.datasets import make_blobs
 
-data = pad.read_csv('data/data_banknote_authentication.txt', sep=",", header=None)
-X, y = mglearn.datasets.make_forge()
-#X, y =make_blobs()
-X_train, X_test, y_train, y_test = train_test_split(X, y)
+data = pad.read_csv('data/data_banknote_authentication.txt', sep=",")
+#X, y = mglearn.datasets.make_forge()
+X = data[['wordcount','sentimentValue']].values
+y = data['Start Rating'].values
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
 #X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=66)
 training_accuracy = []
 test_accuracy = []
 
-neighbors_settings = range(1, 11)
+neighbors_settings = range(1, 20)
 for n_neighbors in neighbors_settings:
   # build the model
   clf = KNeighborsClassifier(n_neighbors=n_neighbors)
@@ -27,8 +29,8 @@ for n_neighbors in neighbors_settings:
   test_accuracy.append(clf.score(X_test, y_test))
 plt.plot(neighbors_settings, training_accuracy, label="training accuracy")
 plt.plot(neighbors_settings, test_accuracy, label="test accuracy")
-plt.ylabel("Accuracy")
-plt.xlabel("vecinos")
+plt.ylabel("Precisión")
+plt.xlabel("Vecinos")
 plt.legend()
 
 # %%
---
+++ test3_tareaIA.py
@@ -0,0 +1,24 @@
+#%%
+from os import sep
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+import matplotlib.patches as mpatches
+#import seaborn as sb
+
+#%matploitlib inline
+
+plt.rcParams['figure.figsize'] = (16 ,9)
+plt.style.use('ggplot')
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+
+datos = pd.read_csv('data/data_banknote_authentication.txt', sep=',')
+datos.hist()
+plt.show()
+# %%
